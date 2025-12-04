c3f98d24a1c7d50a2bda2e8c293600630411477e
Burak Ayen <244418465+EnmAOfficial@users.noreply.github.com>
2025-12-04 23:49:18 +0300

    Update storage.py

--- utils/storage.py
+++ utils/storage.py
@@ -1,1 +1,14 @@
+import json
+import os
 
+def load(path, default):
+    if not os.path.exists(path):
+        return default
+    try:
+        return json.load(open(path, "r", encoding="utf-8"))
+    except:
+        return default
+
+def save(path, data):
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    json.dump(data, open(path, "w", encoding="utf-8"), indent=4, ensure_ascii=False)
